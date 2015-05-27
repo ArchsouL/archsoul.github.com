2b1a6231e5d6d6cf816b6db6e14d08b096387488
alkaid7 <alkaid7@hairang.co.kr>
2015-05-27 11:48:31 +0900

    asdasdasd

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives'])
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: GenericViewCtrl});
     $routeProvider.when('/apm', {templateUrl: 'partials/apm.html', controller: GenericViewCtrl});
-    $routeProvider.when('/lcm', {templateUrl: 'partials/lcm.html', controller: GenericViewCtrl});
+    $routeProvider.when('/lsm', {templateUrl: 'partials/lsm.html', controller: GenericViewCtrl});
     $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: ContactViewCtrl});
     $routeProvider.when('/imprint', {templateUrl: 'partials/imprint.html', controller: GenericViewCtrl});
     $routeProvider.otherwise({redirectTo: '/home'});
